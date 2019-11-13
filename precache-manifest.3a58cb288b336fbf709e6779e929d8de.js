7fee5ad0a0af1c617d82d35f054fed97fc92cc87
Igor <lucifer-angelok@mail.ru>
2019-11-13 18:05:40 +0300

    Updates

---
+++ precache-manifest.3a58cb288b336fbf709e6779e929d8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde3e68ac8e2daa0c9aca66e91b09d5c",
+    "url": "/CV/08f89c84ab0232e9f451.worker.js"
+  },
+  {
+    "revision": "c7d96fd1cde864053fd7b2a1bad12d44",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "72c5081cb6bc4dc9296d",
+    "url": "/CV/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "38e763ccc11e59561242",
+    "url": "/CV/static/css/main.6b8286af.chunk.css"
+  },
+  {
+    "revision": "72c5081cb6bc4dc9296d",
+    "url": "/CV/static/js/2.068aa908.chunk.js"
+  },
+  {
+    "revision": "38e763ccc11e59561242",
+    "url": "/CV/static/js/main.e53c8242.chunk.js"
+  },
+  {
+    "revision": "f933a7bf66f3494e42a6",
+    "url": "/CV/static/js/runtime-main.019e044c.js"
+  }
+]);
