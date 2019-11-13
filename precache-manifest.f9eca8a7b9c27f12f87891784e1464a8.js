a979f35441bf84bc1c22aba835cce34e4a660681
Igor <lucifer-angelok@mail.ru>
2019-11-13 18:15:18 +0300

    Updates

---
+++ precache-manifest.f9eca8a7b9c27f12f87891784e1464a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde3e68ac8e2daa0c9aca66e91b09d5c",
+    "url": "/CV/08f89c84ab0232e9f451.worker.js"
+  },
+  {
+    "revision": "29ded9f5f43e72162e3aa97df8d73b72",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "4b7962f603e91abc8119",
+    "url": "/CV/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "2c19024b9dc4dd1abbd2",
+    "url": "/CV/static/css/main.6b8286af.chunk.css"
+  },
+  {
+    "revision": "4b7962f603e91abc8119",
+    "url": "/CV/static/js/2.50f1e0bc.chunk.js"
+  },
+  {
+    "revision": "2c19024b9dc4dd1abbd2",
+    "url": "/CV/static/js/main.6b59aff0.chunk.js"
+  },
+  {
+    "revision": "92eba6b19af7fab250b9",
+    "url": "/CV/static/js/runtime-main.d96cef18.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CV/precache-manifest.3a58cb288b336fbf709e6779e929d8de.js"
+  "/CV/precache-manifest.f9eca8a7b9c27f12f87891784e1464a8.js"
 );
 
 self.addEventListener('message', (event) => {
